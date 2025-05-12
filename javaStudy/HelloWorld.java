eebd3367abeb82798999be37b5c57b9606dd3df0
seol <fullstack.eunseolgo14@gmail.com>
2025-05-12 17:44:29 +0900

    [고은설] Initial commit

---
+++ javaStudy/HelloWorld.java
@@ -0,0 +1,13 @@
+package javaStudy;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕 나는 고은설이야");
+	}
+	
+	// 문자셋 설정: 설정이 다르면 한글 주석이 깨지는 상황 발생.
+	// 모든 문자셋은 UTF-8로 설정 권장.
+
+}
---
+++ javaStudy/SyntaxEx.java
@@ -0,0 +1,7 @@
+package javaStudy;
+
+public class SyntaxEx {
+
+	
+	
+}
